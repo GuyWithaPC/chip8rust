8cfa7ffc11d5393f4a42fd0d577280dd8f41f0b4
ChaseParate <97500441+ChaseParate@users.noreply.github.com>
2022-11-07 21:55:53 -0700

    Run cargo formatting

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-
-use std::vec;
 use std::path::PathBuf;
+use std::vec;
 
 use olc_pge as olc;
 
-mod instructions;
 mod components;
+mod instructions;
 
-use components::{RAM,Registers};
+use components::{Registers, RAM};
 
 const SCR_W: usize = 64;
 const SCR_H: usize = 32;
@@ -20,15 +19,16 @@ const START_RUN_MODE: RUN = RUN::STEP;
 #[derive(PartialEq)]
 enum RUN {
     CONT,
-    STEP
+    STEP,
 }
 
 impl olc::PGEApplication for Emulator {
     const APP_NAME: &'static str = "Chip8 Emulator - Rust";
 
     fn on_user_create(&mut self, _pge: &mut olc::PixelGameEngine) -> bool {
-        self.ram.load_from_rom(0x000,PathBuf::from("system/font.bin"));
-        self.ram.load_from_rom(0x200,PathBuf::from(ROM_FILE));
+        self.ram
+            .load_from_rom(0x000, PathBuf::from("system/font.bin"));
+        self.ram.load_from_rom(0x200, PathBuf::from(ROM_FILE));
         true
     }
 
@@ -52,7 +52,7 @@ impl olc::PGEApplication for Emulator {
         if self.key_block != 0x10 {
             for i in 0..0x10 as usize {
                 if self.keys[i] {
-                    self.registers.set(self.key_block,i as u8);
+                    self.registers.set(self.key_block, i as u8);
                     self.key_block = 0x10;
                     break;
                 }
@@ -61,41 +61,64 @@ impl olc::PGEApplication for Emulator {
                 return true;
             }
         }
-        if self.run_mode == RUN::CONT { // run continuously at 600 CPS
+        if self.run_mode == RUN::CONT {
+            // run continuously at 600 CPS
             self.cycle_time += delta;
             self.timer_time += delta;
             if self.timer_time >= 1.0 / 60.0 {
                 self.timer = if self.timer == 0 { 0 } else { self.timer - 1 };
-                self.sound_timer = if self.sound_timer == 0 { 0 } else { self.sound_timer - 1 };
+                self.sound_timer = if self.sound_timer == 0 {
+                    0
+                } else {
+                    self.sound_timer - 1
+                };
                 self.timer_time = 0.0;
             }
             if self.cycle_time >= SECONDS_PER_CYCLE {
                 let (redraw, summary) = self.cycle();
-                if redraw { self.draw(pge); }
-                self.draw_debug(pge,summary);
+                if redraw {
+                    self.draw(pge);
+                }
+                self.draw_debug(pge, summary);
                 self.cycle_time = 0.0;
             }
-            if pge.get_key(olc::Key::Space).pressed {self.run_mode = RUN::STEP}
-        } else { // run step-by-step
+            if pge.get_key(olc::Key::Space).pressed {
+                self.run_mode = RUN::STEP
+            }
+        } else {
+            // run step-by-step
             if pge.get_key(olc::Key::Tab).pressed {
-                self.timer_time += 1.0/600.0;
+                self.timer_time += 1.0 / 600.0;
                 if self.timer_time >= 1.0 / 60.0 {
                     self.timer = if self.timer == 0 { 0 } else { self.timer - 1 };
-                    self.sound_timer = if self.sound_timer == 0 { 0 } else { self.sound_timer - 1 };
+                    self.sound_timer = if self.sound_timer == 0 {
+                        0
+                    } else {
+                        self.sound_timer - 1
+                    };
                     self.timer_time = 0.0
                 }
                 let (redraw, summary) = self.cycle();
                 self.draw(pge);
-                self.draw_debug(pge,summary);
+                self.draw_debug(pge, summary);
             }
-            if pge.get_key(olc::Key::Space).pressed {self.run_mode = RUN::CONT}
+            if pge.get_key(olc::Key::Space).pressed {
+                self.run_mode = RUN::CONT
+            }
         }
         true
     }
 }
 
 fn main() {
-    olc::PixelGameEngine::construct(Emulator::new(),(SCR_W + (SCR_W / 2))*DRAW_BIGGER_PIXELS as usize,(SCR_H + (SCR_H / 2))*DRAW_BIGGER_PIXELS as usize,2,2).start();
+    olc::PixelGameEngine::construct(
+        Emulator::new(),
+        (SCR_W + (SCR_W / 2)) * DRAW_BIGGER_PIXELS as usize,
+        (SCR_H + (SCR_H / 2)) * DRAW_BIGGER_PIXELS as usize,
+        2,
+        2,
+    )
+    .start();
 }
 
 struct Emulator {
@@ -114,34 +137,38 @@ struct Emulator {
     run_mode: RUN,
 }
 impl Emulator {
-    fn new () -> Emulator {
+    fn new() -> Emulator {
         Emulator {
             timer_time: 0.0,
             cycle_time: 0.0,
-            display: vec![vec![false;32];64], // x, y format
+            display: vec![vec![false; 32]; 64], // x, y format
             ram: RAM::new(), // using RAM rather than a Vec because it encapsulates ROM loading
-            timer: 0x00, // basic timer
+            timer: 0x00,     // basic timer
             sound_timer: 0x00, // sound timer, plays a beep while > 0
             registers: Registers::new(), // registers 0 through F
             program_counter: 0x200, // programs always start at location 0x200 in RAM
             stack_pointer: 0x000, // doesn't matter where this starts, programs will modify it
             call_stack: Vec::new(),
             key_block: 0x10,
-            keys: vec![false;0x10],
+            keys: vec![false; 0x10],
             run_mode: START_RUN_MODE,
         }
     }
-    fn draw (&mut self, pge: &mut olc::PixelGameEngine) {
-        let bigger_draw = if self.run_mode == RUN::STEP {DRAW_BIGGER_PIXELS} else {DRAW_BIGGER_PIXELS + (DRAW_BIGGER_PIXELS / 2)};
-        let color_on = if self.run_mode == RUN::STEP {
-            olc::Pixel::rgb(0xDF,0x00,0x00)
+    fn draw(&mut self, pge: &mut olc::PixelGameEngine) {
+        let bigger_draw = if self.run_mode == RUN::STEP {
+            DRAW_BIGGER_PIXELS
         } else {
-            olc::Pixel::rgb(0x00,0xDF,0x00)
+            DRAW_BIGGER_PIXELS + (DRAW_BIGGER_PIXELS / 2)
+        };
+        let color_on = if self.run_mode == RUN::STEP {
+            olc::Pixel::rgb(0xDF, 0x00, 0x00)
+        } else {
+            olc::Pixel::rgb(0x00, 0xDF, 0x00)
         };
         let color_off = if self.run_mode == RUN::STEP {
-            olc::Pixel::rgb(0x10,0x00,0x00)
+            olc::Pixel::rgb(0x10, 0x00, 0x00)
         } else {
-            olc::Pixel::rgb(0x00,0x10,0x00)
+            olc::Pixel::rgb(0x00, 0x10, 0x00)
         };
         pge.clear(olc::BLACK);
         for x in 0..64 {
@@ -153,29 +180,53 @@ impl Emulator {
                         } else {
                             color_off
                         };
-                        pge.draw(x as i32 * bigger_draw + xs, y as i32 * bigger_draw + ys, pixel);
+                        pge.draw(
+                            x as i32 * bigger_draw + xs,
+                            y as i32 * bigger_draw + ys,
+                            pixel,
+                        );
                     }
                 }
             }
         }
     }
-    fn draw_debug (&mut self, pge: &mut olc::PixelGameEngine, summary: String) {
+    fn draw_debug(&mut self, pge: &mut olc::PixelGameEngine, summary: String) {
         if self.run_mode == RUN::STEP {
             for i in 0..0x8 {
                 let mut string = String::new();
                 string += format!("R{:1X}:", i).as_str();
                 string += format!("{:2X}", self.registers.get(i as u8)).as_str();
-                pge.draw_string(64 * DRAW_BIGGER_PIXELS + 4, 4 + (i * 8), &string, olc::WHITE);
+                pge.draw_string(
+                    64 * DRAW_BIGGER_PIXELS + 4,
+                    4 + (i * 8),
+                    &string,
+                    olc::WHITE,
+                );
             }
             for i in 0..0x8 {
                 let mut string = String::new();
                 string += format!("R{:1X}:", 0x8 + i).as_str();
                 string += format!("{:2X}", self.registers.get(0x8 + i as u8)).as_str();
-                pge.draw_string(64 * DRAW_BIGGER_PIXELS + 64, 4 + (i * 8), &string, olc::GREY);
+                pge.draw_string(
+                    64 * DRAW_BIGGER_PIXELS + 64,
+                    4 + (i * 8),
+                    &string,
+                    olc::GREY,
+                );
             }
             let (stringa, stringb) = summary.split_once(" => ").unwrap();
-            pge.draw_string(4, 32 * DRAW_BIGGER_PIXELS + 8, &stringa.to_string(), olc::WHITE);
-            pge.draw_string(4, 32 * DRAW_BIGGER_PIXELS + 16, &stringb.to_string(), olc::WHITE);
+            pge.draw_string(
+                4,
+                32 * DRAW_BIGGER_PIXELS + 8,
+                &stringa.to_string(),
+                olc::WHITE,
+            );
+            pge.draw_string(
+                4,
+                32 * DRAW_BIGGER_PIXELS + 16,
+                &stringb.to_string(),
+                olc::WHITE,
+            );
         }
     }
 }
